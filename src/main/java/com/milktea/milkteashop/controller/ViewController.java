6747dd1e5b3fd5f3da4c619bd050b8fe85c3373b
caoxingyu <caoxingyu2016@qq.com>
2018-07-31 15:09:05 +0800

    websocket测试

---
+++ src/main/java/com/milktea/milkteashop/controller/ViewController.java
@@ -0,0 +1,14 @@
+package com.milktea.milkteashop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class ViewController {
+
+   @RequestMapping("/sockettest")
+   public String socketTest(){
+       return "shopWebsocket";
+   }
+}
