0cf69c627f4bfefc5fff20c3031ae378180b126d
曹佳 <BHQH-CXYWB@BHQH-CXYWB-PC>
2018-08-22 15:36:18 +0800

    定时任务

--- src/main/java/com/milktea/milkteashop/config/SchedulingConfig.java
+++ src/main/java/com/milktea/milkteashop/config/SchedulingConfig.java
@@ -4,9 +4,9 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-/*@Configuration
+@Configuration
 @EnableScheduling
-@ComponentScan("com.milktea.milkteashop.job")*/
+@ComponentScan("com.milktea.milkteashop")
 public class SchedulingConfig {
 
 }
