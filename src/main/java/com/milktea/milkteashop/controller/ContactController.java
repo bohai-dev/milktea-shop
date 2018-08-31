69d8b17b1028e6f4ffb0aa01d283c532b3cc4fd0
曹佳 <BHQH-CXYWB@BHQH-CXYWB-PC>
2018-08-31 16:05:14 +0800

    跨域

--- src/main/java/com/milktea/milkteashop/controller/ContactController.java
+++ src/main/java/com/milktea/milkteashop/controller/ContactController.java
@@ -3,6 +3,7 @@ package com.milktea.milkteashop.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,6 +16,7 @@ import com.milktea.milkteashop.vo.ResponseBody;
 import com.milktea.milkteashop.vo.ResponseHeader;
 
 @RestController
+@CrossOrigin
 public class ContactController {
     
     @Autowired
